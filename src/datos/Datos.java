c0c872bbe33ee0f2a975f02be0760a3f7c91d090
marielos <marielos@Toshiba>
2018-04-03 23:15:04 -0600

    Tarea 1 Datos Personales

---
+++ src/datos/Datos.java
@@ -0,0 +1,21 @@
+/*
+Marielos Najarro 00126816
+ */
+package datos;
+
+/**
+ @author marielos
+ */
+public class Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("----Datos personales----");
+        System.out.println("Nombres: Marielos Alejandra");
+        System.out.println("Apellidos: Najarro Alvarez");
+        System.out.println("Edad: 20");
+        System.out.println("Fecha de nacimieto: 06/09/1997");
+    }
+}
